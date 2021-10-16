cc458293cca1478d0e2fd894cf782019e8e47582
joyjoyjoy <81938345+jk-JOY@users.noreply.github.com>
2021-10-16 20:25:32 +0900

    Photon을 제거하고 Mirorr를 얻었다!
    버튼작업... 이제 그만 하고 싶다..

---
+++ Assets/Scripts/CreateRoomUI.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class CreateRoomUI : MonoBehaviour
+{
+
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void CreateRoom()
+    {  // ForUsRoomManager.singleton로 씬에 있는 network매니저를 가져와,
+        //StartHost 함수 호출  == startHOST는 서버를 엶과 동시에
+        //클라이언트로써 게임에 참가하게 만드는 함수
+        var manager = ForUsRoomManager.singleton;
+        //방 설정 작업 처리
+        //
+        //
+        manager.StartHost();
+    }
+}
---
+++ Assets/Scripts/Manager/ForUsRoomManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+/// <summary>
+/// NetworkRoomMANAGER로 상속받았다 _________1
+/// 
+/// 
+/// </summary>
+public class ForUsRoomManager : NetworkRoomManager
+{
+    
+}
+  
